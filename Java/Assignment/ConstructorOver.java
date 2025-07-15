223c2ab27081646f43f0c4de79bb178a703c8257
amalrxj <amalrajkvdev@gmail.com>
2025-07-15 11:20:16 +0530

    jul15a

--- Java/Assignment/ConstructorOver.java
+++ Java/Assignment/ConstructorOver.java
@@ -1,30 +1,33 @@
-class Box{
-double height;
-double width;
-double depth;
-Box(Box obj){
-height = obj.height;
-width = obj.width;
-depth = obj.depth;
+class Box {
+    double height;
+    double width;
+    double depth;
+
+    Box(Box obj) {
+        height = obj.height;
+        width = obj.width;
+        depth = obj.depth;
+    }
+
+    Box(double h, double w, double d) {
+        height = h;
+        width = w;
+        depth = d;
+    }
+
+    double volume() {
+        return width * height * depth;
+    }
 }
-Box(double h, double w, double d){
-height = h;
-width = w;
-depth = d;
-}
-double volume(){
-return width * height * depth;
-}
-}
-class ConstructorOver{
-public static void main(String args[])
-{
-double vol;
-Box box1 = new Box(10, 11, 12);
-Box box2 = new Box(box1);
-vol = box1.volume();
-System.out.println("Volume: "+ vol);
-vol = box2.volume();
-System.out.println("Volume: "+ vol);
-}
+
+class ConstructorOver {
+    public static void main(String args[]) {
+        double vol;
+        Box box1 = new Box(10, 11, 12);
+        Box box2 = new Box(box1);
+        vol = box1.volume();
+        System.out.println("Volume: " + vol);
+        vol = box2.volume();
+        System.out.println("Volume: " + vol);
+    }
 }
